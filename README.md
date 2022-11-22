3ab2c0d8e5574bb7f1e6fe9257c46345140ccfa4
KungFuPanda <kungfupandaunleashed@gmail.com>
2022-11-22 13:27:18 +0530

    assign submit

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Compiler Assingment 
 
-## Name Nishanth 
-## Date Nov 17 2022
+## **Name** Nishanth 
+## **Date** Nov 17 2022
 
-### Moto
-> To create recursive descent parser for grammer 
+### About 
+Recursive decent parser in rust for compiler design course
+
+### Problem
 -----
 > *A  -> B C*
 > 
@@ -27,3 +29,31 @@
 > 
 > *D  -> n  | (A)*
 ----
+
+### Supported Plaforms
+> Only windows and Unix variants (ex. Mac,Unix) supported .
+>
+> For non supported platforms , output is produced but not in correct format 
+
+### Why some platforms not supported ?
+> Because as input for the rogram is taken from the file which may be created in any os
+>
+> Internally program removes every new line character (newline is allowed for readability).
+>
+> But if input doesn't contain any newline, then this program has not restriction
+
+### For rust developers
+> `production` profile is used for less  binary size
+> Tool chain used in release 
+
+*stable-x86_64-pc-windows-msvc (default)*
+
+*rustc 1.65.0 (897e37553 2022-11-02)*
+
+### How to Run (if rust installed)
+1. Clone the repo
+2. Run `cargo run --profile=production -- <input_file_path>`
+
+### How to Run (no rust installed)
+1. Download the binary release.
+2. Run `<binary_release_name> .\<input_file_path>`
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #![allow(non_snake_case)]
 
+#[cfg(windows)]
+const LINE_ENDING:&'static str ="\n";
+#[cfg(not(windows))]
+const LINE_ENDING:&'static str ="\r\n";
+
 /// Recursive descent parser for grammer 
 /// 
 /// A  -> B C
@@ -19,25 +24,30 @@ use lexer::Lexer;
 use lexer::RecDes;
 use std::collections::HashMap;
 use std::{fs, io::Read};
+use std::env;
+use std::path;
 
-fn ReadFileIntoStringVec(fileName: &String, sep: char) -> Vec<String> {
+fn ReadFileIntoStringVec(filePath: &String, sep: char) -> Vec<String> {
     let mut file = fs::OpenOptions::new()
         .read(true)
         .write(false)
         .append(false)
-        .open(fileName)
+        .open(filePath)
         .ok()
         .unwrap();
 
     let mut content = String::new();
 
     _ = file.read_to_string(&mut content);
-    content = String::from(content.trim());
+    content = String::from(content.replace(LINE_ENDING, ""));
 
     let filesub: Vec<_> = content.split(sep).collect();
     let mut res: Vec<String> = Vec::new();
 
     for value in filesub {
+        if value.eq("") {
+            continue;
+        }
         res.push(String::from(value));
     }
 
@@ -55,8 +65,21 @@ fn genSpace(count: usize) -> String {
 }
 
 fn main() {
-    let fileName = String::from("./examples/input.txt");
-    let fileContents = ReadFileIntoStringVec(&fileName, ';');
+    let argVec:Vec<String> = env::args().collect();
+
+    if argVec.len() < 2 {
+        panic!("path to input file not passed");
+    }
+
+    let inputPathString = &argVec[1];
+    let inputPath = path::Path::new(inputPathString);
+
+    if !inputPath.exists() {
+        panic!("path to input file doesn't exists");
+    }
+
+    let filePath:String = String::from(inputPath.as_os_str().to_str().unwrap());
+    let fileContents = ReadFileIntoStringVec(&filePath, ';');
 
     let mut valueMap: HashMap<&String, bool> = HashMap::new();
     let mut lexer: Lexer;
